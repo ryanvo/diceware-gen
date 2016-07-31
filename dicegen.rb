30700d8e472c144e04343c0b76144446210d6a50
Ryan Vo <ryanvo@seas.upenn.edu>
2016-07-31 03:35:20 -0500

    add dicegen

--- dicegen.rb
+++ dicegen.rb
@@ -1,5 +1,33 @@
 class Dicegen
 
+  def initialize(filename, random_word_service)
+    @filename = File.new(filename)
+    @random_word_service = random_word_service
+  end
 
+  def generate(num_of_phrases, num_of_passwords)
+    raise ArgumentError, 'num_of_phrases must be > 0' unless num_of_phrases > 0
+    raise ArgumentError, 'num_of_passwords must be > 0' unless num_of_passwords > 0
+
+    passes = []
+
+    num_of_passwords.times do
+      passes.push(new_pass(num_of_phrases))
+    end
+
+    passes
+  end
+
+  def new_pass(num_of_phrases)
+    password = ''
+    num_of_phrases.times do
+      password << get_random_word.to_s << ' '
+    end
+    password.chop
+  end
+
+  def get_random_word
+    @random_word_service.getword
+  end
 
 end
