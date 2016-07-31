8292533ebf2a92ec8bbf608c8f7ffd66deed35ec
Ryan Vo <ryanvo@seas.upenn.edu>
2016-07-31 14:33:38 -0500

    update test to eliminate dependency on other class methods

--- spec/password_file_loader_spec.rb
+++ spec/password_file_loader_spec.rb
@@ -3,34 +3,28 @@ require_relative '../password_file_loader.rb'
 
 describe PasswordFileLoader do
 
-  let(:file) { double('file', :readline => @nextline) }
-  let(:pfl) { PasswordFileLoader.new(file) }
-
-  describe '.get_line' do
-    it 'should return a line' do
-      @nextline = 'expected'
-      expect(pfl.get_line).to eql(@nextline)
-    end
-  end
+  let(:file) { double('file') }
+  let(:underTest) { PasswordFileLoader.new(file) }
 
   describe '.create_pair' do
     it 'should split the string into a pair' do
-      @nextline = '11131	aide'
-      expect(pfl.create_pair(@nextline)).to eql([:'11131', :aide])
+      expect(underTest.create_pair('11131	aide')).to eql(['11131', 'aide'])
     end
   end
 
   describe '.generate_hash' do
-
     context 'for each line' do
       it 'should add a new pair to the hash' do
-        @nextline = '11131 aide'
-        map = pfl.generate_hash
-        expect(map).to include(:"11131" => :aide)
-        expect(map.size).to eql(1)
+        allow(file).to receive(:each_line).and_yield('11131  aide').and_yield('11132 aow')
+        expect(underTest).to receive(:create_pair).exactly(2).times.and_return([11131, 'aide'], [11132, 'aow'])
+
+        result = underTest.generate_hash
+
+        expect(result).to include(:'11131' => :aide)
+        expect(result).to include(:'11132' => :aow)
+        expect(result.size).to eql(2)
       end
     end
-
   end
 
 end
